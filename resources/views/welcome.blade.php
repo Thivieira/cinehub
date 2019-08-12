70e9743d69cc5813848958c31b5365ce873a2597
Thiago Alves Vieira <thiago.a.vieira@hotmail.com>
2019-08-12 01:36:04 -0300

    fixing css not being served in production

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -26,11 +26,7 @@
         <title>{{ config('app.name') }}</title>
         <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:400,900&display=swap" rel="stylesheet">
         <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-
-        <!-- Fonts -->
-
-
-        <!-- Styles -->
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/app.min.css') }}">
 
     </head>
     <body>
